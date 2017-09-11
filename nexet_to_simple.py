bd54d76998d21caf8030e269b2485fc8aca66aa1
Jeff <writetojeffrey@gmail.com>
2017-09-10 19:15:01 -0700

    Convert nexet train boxes to simple parser format. Add script to train frcnn on nexet data.

---
+++ nexet_to_simple.py
@@ -0,0 +1,28 @@
+from __future__ import print_function
+import os
+
+def nexet_to_simple(train_boxes_csv, train_folder, simple_filename):
+    assert(os.path.exists(train_folder))
+
+    with open(train_boxes_csv, 'r') as read_f:
+        with open(simple_filename, 'w') as write_f:
+            for line in read_f:
+                line_split = line.strip().split(',')
+                (filename, x1, y1, x2, y2, class_name, confidence) = line_split
+                fullpath = os.path.join(train_folder, filename)
+                if os.path.exists(fullpath):
+                    if ',' in fullpath:
+                        print('warning path contains comma:', fullpath, 'line:', line)
+                    else:
+                        simple_line = '{0},{1},{2},{3},{4},{5}'.format(fullpath, int(float(x1)), int(float(y1)), int(float(x2)), int(float(y2)), class_name)
+                        print(simple_line, file = write_f)
+                else:
+                    print('warning non-existent file:', fullpath, 'line:', line)
+
+def convert():
+    nexet_to_simple('/home/eljefec/data/nexet/train_boxes.csv',
+                    '/home/eljefec/data/nexet/train',
+                    '/home/eljefec/data/nexet/train_boxes.simple.csv')
+
+if __name__ == '__main__':
+    convert()
