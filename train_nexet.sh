09ae8344915362311b85748e685092fe09e8ee46
Jeff <writetojeffrey@gmail.com>
2017-09-14 01:37:17 -0700

    Include horizontal flips when training.

--- train_nexet.sh
+++ train_nexet.sh
@@ -1,3 +1,3 @@
 [ ! -f resnet50_weights_tf_dim_ordering_tf_kernels.h5 ] && wget https://github.com/fchollet/deep-learning-models/releases/download/v0.2/resnet50_weights_tf_dim_ordering_tf_kernels.h5
 
-python train_frcnn.py -o simple -p ~/data/nexet/train_boxes.simple.csv
+python train_frcnn.py -o simple -p ~/data/nexet/train_boxes.simple.csv --hf
