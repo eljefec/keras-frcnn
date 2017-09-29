6ae9ea72f9f8885df60ddff843c2941b58a4d7ca
Jeff <writetojeffrey@gmail.com>
2017-09-28 21:40:20 -0700

    Train starting from good model.

--- train_nexet.sh
+++ train_nexet.sh
@@ -1,3 +1,3 @@
 [ ! -f resnet50_weights_tf_dim_ordering_tf_kernels.h5 ] && wget https://github.com/fchollet/deep-learning-models/releases/download/v0.2/resnet50_weights_tf_dim_ordering_tf_kernels.h5
 
-python train_frcnn.py -o pascal_voc -p ~/data/ --hf
+python train_frcnn.py -o pascal_voc -p ~/data/ --hf --input_weight_path ~/data/nexet/frcnn/1-hf_nosmall/model_frcnn_e347_l0.309_a0.916.hdf5
